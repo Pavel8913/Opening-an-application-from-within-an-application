260f0b3afc073d68d2e2161d356e23292edeaed7
Pavel <pavel.en00@mail.ru>
2024-02-03 15:08:43 +0700

    Add files via upload

---
+++ Открытие стороннего приложения/Открытие стороннего приложения/Открытие стороннего приложения.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+    system("\"D:\\VS 2022\\Common7\\IDE\\devenv.exe\"");
+    cout << "VS успешно открыт!";
+
+    for (;;) {}
+    return 0;
+}
+
+
